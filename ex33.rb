edef818985e0c3feb2dfedd9754188d454b5ea63
pengquankun <283857115@qq.com>
2012-03-13 00:11:12 +0800

    added ex33 while-loop

---
+++ ex33.rb
@@ -0,0 +1,18 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i = i + 1
+  puts "Numbers now: #{numbers}"
+  puts "At the bottom i is #{i}"
+  puts "\n"
+end
+
+puts "The numbers: "
+
+for num in numbers
+  puts num
+end
