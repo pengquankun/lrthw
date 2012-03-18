4d5119992b18df30b0c7e1f19db401386ecf43fb
pengquankun <283857115@qq.com>
2012-03-18 17:38:46 +0800

    added game.rb in exercise 42

--- projects/gothonweb/lib/gothonweb.rb
+++ projects/gothonweb/lib/gothonweb.rb
@@ -1,10 +1,21 @@
 require_relative "gothonweb/version"
 require "sinatra"
+require "erb"
 
 module Gothonweb
+
   get '/' do
     greeting = "Hello, World!"
     erb :index, :locals => {:greeting => greeting}
   end
-end
 
+  get '/hello' do
+    erb :hello_form
+  end
+
+  post '/hello' do
+    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}"
+    erb :index, :locals => {:greeting => greeting}
+  end
+
+end
